8c6b71661c60f96020ce5de444916987935a84f9
Astatine <87228325+cpu-astatine@users.noreply.github.com>
2024-05-17 03:27:28 -0400

    Create anaprogram.py

---
+++ anaprogram.py
@@ -0,0 +1,29 @@
+# rastgele-sayilar Python Programi
+# Kodlayan: cpu-astatine
+
+# Kütüphanelerin Eklenmesi
+import os
+import random
+import datetime
+import time
+
+# Fonksiyonlarin Yazilmasi
+def clear():
+    os.system("clear")
+def sayiUret():
+    sayi = random.randint(1,1000)
+    print(f"Uretilen rastgele bir sayi: {sayi}")
+    time.sleep(0.5)
+
+# Ana Programin Yazilmasi
+sistem_zamani = datetime.datetime.now()
+clear()
+print("rastgele-sayilar V1")
+print("Kodlayan: cpu-astatine")
+print(f"Sistem Zamani: {sistem_zamani}")
+while True:
+    try:
+        sayiUret()
+    except KeyboardInterrupt:
+        print("\nProgramdan cikis yaptiniz!")
+        break
